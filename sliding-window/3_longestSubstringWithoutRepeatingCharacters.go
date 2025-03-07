1796582f9d1199853bfbb4a4430d75d445dd7350
roman <rpmisko@gmail.com>
2025-03-07 10:43:33 +0300

    Solved 3

--- sliding-window/3_longestSubstringWithoutRepeatingCharacters.go
+++ sliding-window/3_longestSubstringWithoutRepeatingCharacters.go
@@ -1,23 +1,23 @@
 package main
 
-import "fmt"
-
 func lengthOfLongestSubstring(s string) int {
-
-	longestLength := 0
-	for i := 0; i < len(s); {
-		hash := make(map[byte]bool)
-		for i < len(s) && !hash[s[i]] {
-			hash[s[i]] = true
-			i++
+	set := make(map[byte]bool, 0)
+	maxString := 0
+	l, r := 0, 0
+	for r < len(s) {
+		if set[s[r]] {
+			for s[r] != s[l] {
+				set[s[l]] = false
+				l++
+			}
+			l++
+		} else {
+			set[s[r]] = true
 		}
-		if len(hash) > longestLength {
-			longestLength = len(hash)
+		if maxString < r-l+1 {
+			maxString = r - l + 1
 		}
+		r++
 	}
-	return longestLength
-}
-
-func main() {
-	fmt.Println(lengthOfLongestSubstring("dfvdf"))
+	return maxString
 }
